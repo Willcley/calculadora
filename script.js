acbc6527d28fcbe28d7e73cd39955c8cccb83df8
Willcley Melo <willcley2003@gmail.com>
2023-04-15 20:57:08 -0300

    Adicionado: função inativa extra

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 function clean() {
-
+    alert('Em breve...');
 }
 
 function back() {
-
+    alert('Em breve...');
 }
 
 function insert(a) {
-
+    alert('Em breve...');
 }
 
 function calcular() {
+    alert('Em breve...');
+}
 
+function mudaTema() {
+    alert('Em breve...');
+}
+
+function mudaFundo() {
+    alert('Em breve...');
 }
